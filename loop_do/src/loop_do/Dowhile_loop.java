e6fa68e76fc827bf8e2bb9a0131b47733d0b14a0
prath <prath@DESKTOP-E0LD31N>
2022-11-01 18:19:41 +0530

    second commit

---
+++ loop_do/src/loop_do/Dowhile_loop.java
@@ -0,0 +1,14 @@
+package loop_do;
+
+public class Dowhile_loop {
+
+	public static void main(String[] args) {
+		int i=1,j=1;
+		do {
+			i++;
+			j++;
+			System.out.println("i="+i+"and j="+j);
+		}while(i<10||j<10);
+		}
+
+}
