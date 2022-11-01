655dab80b3c68072b0951379e16355eaa94086e7
prath <prath@DESKTOP-E0LD31N>
2022-11-01 11:07:04 +0530

    third commit

---
+++ decisionmaking_operator/src/decisionmaking_operator/Hello.java
@@ -0,0 +1,23 @@
+package decisionmaking_operator;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int x=8,y=7;
+		if(x >=y)
+		{
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+			
+
+	}
+
+}
---
+++ decisionmaking_operator/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author prath
+ *
+ */
+module decisionmaking_operator {
+}
