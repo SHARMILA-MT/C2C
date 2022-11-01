0caddad6f916a76c86e81542700a7ba7d813e444
prath <prath@DESKTOP-E0LD31N>
2022-11-01 10:33:16 +0530

    Second commit

---
+++ arithmetic_operator/src/arithmetic_operator/Hello.java
@@ -0,0 +1,18 @@
+package arithmetic_operator;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+             int x= 10,y=20;
+             System.out.println(x+y);
+             System.out.println(--x);
+             System.out.println(++y);
+
+	}
+
+}
---
+++ arithmetic_operator/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author prath
+ *
+ */
+module arithmetic_operator {
+}
