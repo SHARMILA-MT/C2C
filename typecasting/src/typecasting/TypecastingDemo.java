5ae19eebdf066b8db8f62715c2694c681ed02429
prath <prath@DESKTOP-E0LD31N>
2022-11-01 12:03:29 +0530

    first commit

---
+++ typecasting/src/typecasting/TypecastingDemo.java
@@ -0,0 +1,18 @@
+package typecasting;
+
+public class TypecastingDemo {
+
+	public TypecastingDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		float f=10.532f;
+		long l=(long)f;
+		double d=f;
+		byte b=12;
+		short s=b;
+		int a=1;
+		System.out.println(l);
+			}
+}
