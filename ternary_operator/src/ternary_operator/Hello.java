9672b6241e8430fb5779ba7873a2f881f5a81499
prath <prath@DESKTOP-E0LD31N>
2022-11-01 10:42:09 +0530

    Second commit

---
+++ ternary_operator/src/ternary_operator/Hello.java
@@ -0,0 +1,16 @@
+package ternary_operator;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int x=10;
+		x=(10==x)?1:0;
+		System.out.println(x);
+
+	}
+
+}
