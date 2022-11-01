50740e1402048d0189d36e18c7316b26c8d6672a
prath <prath@DESKTOP-E0LD31N>
2022-11-01 12:20:04 +0530

    first commit

---
+++ unicode/src/unicode/Hello.java
@@ -0,0 +1,16 @@
+package unicode;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		boolean var = false;
+		System.out.println(var);
+		char var1 = '\u00A7';
+		System.out.println(var1);
+	}
+
+}
