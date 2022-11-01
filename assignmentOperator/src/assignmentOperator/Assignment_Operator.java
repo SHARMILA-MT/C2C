7ae33cc9d3936c55c8ce6112660b697a4580d93d
prath <prath@DESKTOP-E0LD31N>
2022-11-01 10:24:16 +0530

    first commit

---
+++ assignmentOperator/src/assignmentOperator/Assignment_Operator.java
@@ -0,0 +1,21 @@
+package assignmentOperator;
+
+public class Assignment_Operator {
+
+	public Assignment_Operator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int x =4;
+		x+=5;
+		x-=5;
+		x*=5;
+		x/=4;
+		x%=5;
+		System.out.println(x);
+		
+
+	}
+
+}
