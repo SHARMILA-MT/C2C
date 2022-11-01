5093c17bb4073e52feb6c7d9f1538c048349b2c8
prath <prath@DESKTOP-E0LD31N>
2022-11-01 18:32:05 +0530

    third commit

---
+++ while_loop/src/while_loop/While_loop.java
@@ -0,0 +1,19 @@
+package while_loop;
+
+public class While_loop {
+
+	public static void main(String[] args) {
+		int i=1,j=1;
+		while(i<10 || j<10);
+		{
+			i++;
+			j++;
+		System.out.println("i="+i+"and j="+j);
+			}
+		}
+			
+			
+
+	}
+
+}
